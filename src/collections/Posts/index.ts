5c478be6e31df787502045328d133122d42db215
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-11-16 18:38:32 -0300

    feat(ar): posts

--- src/collections/Posts/index.ts
+++ src/collections/Posts/index.ts
@@ -95,6 +95,7 @@ export const Posts: CollectionConfig<'posts'> = {
             {
               name: 'content',
               type: 'richText',
+              localized: true,
               editor: lexicalEditor({
                 features: ({ rootFeatures }) => {
                   return [
